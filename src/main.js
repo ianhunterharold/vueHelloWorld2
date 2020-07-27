c0174cd361a24879657e0c59c37e1f57548eea1d
ianhunterharold <ianhunterharold@gmail.com>
2020-07-27 16:09:39 -0600

    wip

--- src/main.js
+++ src/main.js
@@ -4,5 +4,13 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  data: {
+    results: [
+      {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
+      {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
+      {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
+      {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
+    ]
+  },
   render: h => h(App)
 }).$mount('#app')
