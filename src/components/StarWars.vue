102c6f91d0bc651b0d537c1001aea8c4ea30bf8f
ianhunterharold <ianhunterharold@gmail.com>
2020-07-29 11:01:56 -0600

    wip

--- src/components/StarWars.vue
+++ src/components/StarWars.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <h1>Star Wars!</h1>
-
-   <div v-for="planet in starwars.results" :key="planet.name">
+  <h1>Star Wars!</h1>
+  <div v-for="(planet) in starwars.results" :key="planet.name">
+  <h5>Name</h5>
     {{planet.name}}
-   </div>
+  <h5>Full Rotation</h5>
+    {{planet.rotation_period}}
+  <h5>Days in Year</h5>
+    {{planet.orbital_period}}
+  <h5>Climate</h5>
+    {{planet.climate}}
+  <h5>Terrain</h5>
+    {{planet.terrain}}
+   <h5>Population</h5>
+    {{planet.population}}
+  </div>
   
   </div>
 </template>
