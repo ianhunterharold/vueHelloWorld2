376907097b294bbd36c8b13caecf8f94fb26b509
ianhunterharold <ianhunterharold@gmail.com>
2020-07-29 10:52:47 -0600

    understood how to iterate through a nested list

--- src/components/StarWars.vue
+++ src/components/StarWars.vue
@@ -1,11 +1,14 @@
-
-
 <template>
   <div>
-      <h1>Star Wars!</h1>
-      {{starwars[0].name}}
+    <h1>Star Wars!</h1>
+
+   <div v-for="planet in starwars.results" :key="planet.name">
+    {{planet.name}}
+   </div>
+  
   </div>
 </template>
+
 
 <script>
 export default {
@@ -17,7 +20,7 @@ data(){
   },
   methods: {
     getData(){
-    fetch(`https://swapi.dev/api/people/10`)
+    fetch(`https://swapi.dev/api/planets`)
     .then(r => r.json() ) 
     .then(response => this.starwars = response)
     }
